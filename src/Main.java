a4490eb0ad3af47415f2178fd415513715f7406f
Ishita Agarwal <agarishita2907@gmail.com>
2025-01-09 20:07:38 +0530

    add modified changes in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int sum =5+10;
+        int a=20;
+        int sum =5+10+20;
         System.out.println("Hello world! This is fir git push" +sum);
     }
 }
